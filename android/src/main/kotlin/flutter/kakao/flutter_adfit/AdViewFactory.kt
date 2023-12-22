d24cb2b0010dc6a9e84709989eb25fbb7bb2477b
jeonlego012 <jyo9873@gmail.com>
2023-12-22 15:56:05 +0900

    [fix]fix instantiate

--- android/src/main/kotlin/flutter/kakao/flutter_adfit/AdViewFactory.kt
+++ android/src/main/kotlin/flutter/kakao/flutter_adfit/AdViewFactory.kt
@@ -31,7 +31,7 @@ abstract class AdViewFactory
          * Flutter Android v1 API (using Registrar)
          */
         fun registerWith(registrar: Registrar): AdViewFactory {
-            val plugin = object: AdViewFactory(registrar.messenger(), registrar.context())
+            val plugin : AdViewFactory = AdViewFactory(registrar.messenger(), registrar.context())
             registrar.platformViewRegistry().registerViewFactory("flutter.kakao.adfit/AdFitView", plugin)
             registrar.addViewDestroyListener {
                 plugin.onDestroy()
@@ -44,7 +44,7 @@ abstract class AdViewFactory
          * Flutter Android v2 API (using FlutterPluginBinding)
          */
         fun registerWith(flutterPluginBinding: FlutterPluginBinding): AdViewFactory {
-            val plugin = object: AdViewFactory(flutterPluginBinding.binaryMessenger,
+            val plugin : AdViewFactory = AdViewFactory(flutterPluginBinding.binaryMessenger,
                     flutterPluginBinding.applicationContext)
             flutterPluginBinding.platformViewRegistry.registerViewFactory(
                     "flutter.kakao.adfit/AdFitView", plugin)
