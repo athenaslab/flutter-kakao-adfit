26c42a8b4c6573f3709a326d0f9d65c1d8d9326d
jeonlego012 <jyo9873@gmail.com>
2023-12-22 16:00:04 +0900

    [fix]remove abstract

--- android/src/main/kotlin/flutter/kakao/flutter_adfit/AdViewFactory.kt
+++ android/src/main/kotlin/flutter/kakao/flutter_adfit/AdViewFactory.kt
@@ -8,7 +8,7 @@ import io.flutter.plugin.common.JSONMessageCodec
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import io.flutter.plugin.platform.PlatformViewFactory
 
-abstract class AdViewFactory
+class AdViewFactory
     private constructor(private val messenger: BinaryMessenger, private val appContext: Context)
     : PlatformViewFactory(JSONMessageCodec.INSTANCE) {
 
